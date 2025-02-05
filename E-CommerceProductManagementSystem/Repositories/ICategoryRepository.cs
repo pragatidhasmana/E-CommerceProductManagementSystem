c69d604b41d41e8176922fa9280a3a30e0c7d5ef
Pragati <pragati.dhasmana99@gmail.com>
2025-02-05 18:50:51 +0530

    Added basic repo interface and categorycontroller

--- E-CommerceProductManagementSystem/Repositories/ICategoryRepository.cs
+++ E-CommerceProductManagementSystem/Repositories/ICategoryRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace E_CommerceProductManagementSystem.Repositories
+﻿using E_CommerceProductManagementSystem.Models;
+
+namespace E_CommerceProductManagementSystem.Repositories
 {
     public interface ICategoryRepository
     {
+        Task<IEnumerable<Category>> GetAllCategories();
+        Task<Category> GetCategoryById(int id);
+        Task<Category> AddCategory(Category product);
+        Task<Category> UpdateCategory(Category product);
+        Task<bool> DeleteCategory(int id);
+
     }
 }
