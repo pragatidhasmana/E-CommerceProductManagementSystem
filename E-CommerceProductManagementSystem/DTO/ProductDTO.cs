ba90233a3beda5bc57c5677f839aed19108331df
Pragati <pragati.dhasmana99@gmail.com>
2025-02-09 14:46:37 +0530

    updated product dto

--- E-CommerceProductManagementSystem/DTO/ProductDTO.cs
+++ E-CommerceProductManagementSystem/DTO/ProductDTO.cs
@@ -2,12 +2,13 @@
 {
     public class ProductDTO
     {
-               
+
+        public int ProductId { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
         public decimal Price { get; set; }
-        public int Stock { get; set; }  
-
+        public int Stock { get; set; }
+        public int CategoryId { get; set; }
         public string? CategoryName { get; set; }
 
     }
